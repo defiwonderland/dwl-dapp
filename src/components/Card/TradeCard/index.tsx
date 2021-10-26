426573529c4bb738a4981f5b0d47e099a612583f
Michael <wuxueyuan10@gmail.com>
2021-10-26 12:39:12 +1100

    fix wndr image in trade page

--- src/components/Card/TradeCard/index.tsx
+++ src/components/Card/TradeCard/index.tsx
@@ -71,7 +71,7 @@ const TradeCard: React.FC = () => {
                             justifyContent: "start",
                             width: "200px",
                         }}>
-                            <CustomImg src="./images/trades/WNDR.png" />
+                            <CustomImg src="./images/trades/wndr.png" />
 
                             <Box>
                                 <CardText ml="15px" mt="5px" mb="5px" fontWeight={600} color="#ffffff">To</CardText>
